0e8b41a793cee5ff17fde486ab5399d08d127c41
Aung <taung@cgsh.com>
2018-04-30 14:39:58 -0400

    initial

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const merge = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const parts = require('./webpack.parts');
+
+const path = require('path');
+const PATHS = {
+    app: path.join(__dirname, 'app'),
+    build: path.join(__dirname, 'build'),
+};
+
+
+const commonConfig= merge([
+    {
+        entry: {
+            app: PATHS.app,
+        },
+        output: {
+            path: PATHS.build,
+            filename: '[name].js',
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: 'Webpack demo',
+            }),
+        ],
+    }
+]);
+
+const productionConfig = merge([]);
+
+const developmentConfig = merge([
+    parts.devServer({
+        // Customize host/port here if needed
+        host: process.env.HOST,
+        port: process.env.PORT,
+      }),
+    part.loadCSS()
+      
+])
+
+module.exports = mode=>{
+    if(mode === 'production' ){
+        return merge(commonConfig, productionConfig, {mode})
+    }
+
+    return merge(commonConfig, developmentConfig, { mode });
+}
+
