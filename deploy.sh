1ba12f95879fba27c517f0ea0f518f07d3e3dc55
Aung <taung@cgsh.com>
2018-05-01 17:37:40 -0400

    encrypting

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #deploy with scp
-scp -C -i ./id_rsa -r ./build/* taung@utest.eastus.cloudapp.azure.com:/var/www/html
+scp -C -r ./build/* taung@utest.eastus.cloudapp.azure.com:/var/www/html
