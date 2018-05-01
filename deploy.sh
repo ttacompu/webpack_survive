1a748cfbeedc03f6e5c3b55ee15a14360e032c52
Aung <taung@cgsh.com>
2018-05-01 15:12:41 -0400

    deploy again!

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 #deploy with scp
-scp -C -i ./id_rsa  -r ./build/* taung@utest.eastus.cloudapp.azure.com:/var/www/html
+eval "$(ssh-agent -s)" # Start ssh-agent cache
+chmod 600 ./id_rsa # Allow read access to the private key
+ssh-add ./id_rsa # Add the private key to SSH
+scp -C -r ./build/* taung@utest.eastus.cloudapp.azure.com:/var/www/html
