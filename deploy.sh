9d9ef68f9898b883687148ec36942fea285d9a8e
Aung <taung@cgsh.com>
2018-05-01 15:40:43 -0400

    last commit

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #deploy with scp
 
-scp -r ./build/* taung@utest.eastus.cloudapp.azure.com:/var/www/html
+scp -C -i ./id_rsa -r ./build/* taung@utest.eastus.cloudapp.azure.com:/var/www/html
