b3b57428083867cb8a4d6db9f06c6034e51cda69
ttacompu <ttacompu@gmail.com>
2018-05-05 13:10:40 -0400

    changes

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 #deploy with scp
-scp -C -r ./build/* taung@utest.eastus.cloudapp.azure.com:/var/www/html
-
+scp -C -i -r /tmp/deploy_rsa ./build/* taung@utest.eastus.cloudapp.azure.com:/var/www/html
