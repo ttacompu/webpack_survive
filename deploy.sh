75eff15cb6eb998dcea20b5a9a73d87b972ec725
ttacompu <ttacompu@gmail.com>
2018-05-05 12:42:41 -0400

    tired

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #deploy with scp
-scp -C -r ./build/* taung@utest.eastus.cloudapp.azure.com:/var/www/html
+#scp -C -r ./build/* taung@utest.eastus.cloudapp.azure.com:/var/www/html
+return 0;
