cf26f0196ea25f1775b94bec5eac74e2c233c23c
Aung <taung@cgsh.com>
2018-06-07 13:26:08 -0400

    removed files

--- app/component.js
+++
@@ -1,5 +0,0 @@
-export default (text = 'from windows!') =>{
-const element = document.createElement('div');
-element.innerHTML = text;
-return element;
-}
--- app/scripts/deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#deploy with scp
-scp -C  -r -q ./build/* taung@utest.eastus.cloudapp.azure.com:/var/www/html
